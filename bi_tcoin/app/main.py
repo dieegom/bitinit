a0a4653a48ac897d916f99db4121718f202b47c1
dieegom <diego.mariano@birmind.com.br>
2017-02-04 20:22:44 -0200

    mais coisa

--- bi_tcoin/app/main.py
+++ bi_tcoin/app/main.py
@@ -16,3 +16,26 @@ def get_cambio():
         return a.json()
     except:
         print("sem conexão")
+
+def get_chart():
+    try:
+        a = requests.get("https://api.blockchain.info/charts/$chartName?timespan=$timespan&rollingAverage=$rollingAverage&start=$start&format=$format&sampled=$sampled")
+
+#ex: https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json
+#$timespan Duration of the chart, default is 1 year for most charts, 1 week for mempool charts. (Optional)
+#$rollingAverage Duration over which the data should be averaged. (Optional)
+#$start Datetime at which to start the chart. (Optional)
+#$format Either JSON or CSV, defaults to JSON. (Optional)
+#$sampled Boolean set to 'true' or 'false' (default 'true'). If true, limits the number of datapoints returned to ~1.5k for performance reasons. (Optional)
+#Please note that values for charts can be represented in scientific notation (14,627,700 is represented as 1.46277E7)
+
+        return a.json()
+    except:
+        print("sem conexão")
+
+def get_mkt_share(timespan):
+    try:
+        a = requests.get("https://api.blockchain.info/pools?timespan="+timespan)
+        return a.json()
+    except:
+        print("sem conexão")
--- bi_tcoin/app/views.py
+++ bi_tcoin/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app.main import get_stat, get_cambio
+from app.main import get_stat, get_cambio, get_mkt_share
 
 # Create your views here.
 
@@ -16,6 +16,9 @@ def bitinit(request):
     try:
         context = {"bitcoin": get_stat(),
                    "cambio": get_cambio(),
+                   "mkt_shr_1d": get_mkt_share("1days"),
+                   "mkt_shr_5d": get_mkt_share("5days"),
+                   "mkt_shr_10d": get_mkt_share("10days")
                    }
         return render(request,'bitinit.html',context)
     except:
