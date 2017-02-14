9efb75aa9f99d86613f719aab32aa5c11af15071
dieegom <diego.mariano@birmind.com.br>
2017-02-13 23:20:28 -0200

    aeaeae

--- bi_tcoin/app/main.py
+++ bi_tcoin/app/main.py
@@ -1,4 +1,5 @@
 import datetime
+import operator
 import traceback
 
 import requests
@@ -23,22 +24,30 @@ def get_cambio():
 def get_chart(chart):
     try:
         a = requests.get(chart)
-
-#ex: https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json
-#$timespan Duration of the chart, default is 1 year for most charts, 1 week for mempool charts. (Optional)
-#$rollingAverage Duration over which the data should be averaged. (Optional)
-#$start Datetime at which to start the chart. (Optional)
-#$format Either JSON or CSV, defaults to JSON. (Optional)
-#$sampled Boolean set to 'true' or 'false' (default 'true'). If true, limits the number of datapoints returned to ~1.5k for performance reasons. (Optional)
-#Please note that values for charts can be represented in scientific notation (14,627,700 is represented as 1.46277E7)
         return a.json()
+    except:
+        print("sem conexão")
+
+def get_chart_ord(chart):
+    try:
+        a = requests.get(chart)
+        b = a.json()
+        c = []
+        for item in b['values']:
+            c.append([item['x'], item['y']])
+        return c
     except:
         print("sem conexão")
 
 def get_mkt_share(timespan):
     try:
         a = requests.get("https://api.blockchain.info/pools?timespan="+timespan)
-        return a.json()
+        b = a.json()
+        s = [[k,v] for v,k in sorted(
+            [[v,k] for k,v in b.items()],reverse=True
+            )
+            ]
+        return s
     except:
         print("sem conexão")
 
@@ -46,17 +55,69 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
-def get_get():
-    a=[]
+def get_cambio(moeda,weeks):
+    a = []
     try:
-        date_init = "2017-01-01"
         date_end = datetime.datetime.today()
-        start = datetime.datetime.strptime(date_init, "%Y-%m-%d")
-
+        date_init = date_end - datetime.timedelta(days=7*weeks)
+        start = date_init
         while start < date_end:
-            a.append(requests.get("http://api.fixer.io/"+start.strftime('%Y-%m-%d')+"?symbols=CNY&base=USD").json())
+            a.append(requests.get("http://api.fixer.io/"+start.strftime('%Y-%m-%d')+"?symbols="+moeda+"&base=USD").json())
             start = start + datetime.timedelta(days=1)
-        return a
+        c = []
+        for item in a:
+            c.append([item['date'],item['rates'][moeda]])
+        return c
+
     except:
         print(traceback.print_exc(file=sys.stdout))
         return "None"
+
+def get_floating_cny():
+
+    try:
+        cny = get_cambio("CNY",1)
+        bitcoin = get_chart_ord("https://api.blockchain.info/charts/market-price?timespan=1weeks&rollingAverage=1hours&format=json")
+
+        floater = [['Dias', 'Bitcoins em USD', 'CNY em USD'],]
+
+        for cont in range(0, len(bitcoin)):
+            floater.append([cny[cont][0], bitcoin[cont][1], cny[cont][1]*1000.0])
+
+        return floater
+
+    except:
+        print("f#$%#@ck")
+
+def get_floating_blr():
+
+    try:
+        brl = get_cambio("BRL",1)
+        bitcoin = get_chart_ord("https://api.blockchain.info/charts/market-price?timespan=1weeks&rollingAverage=1hours&format=json")
+
+        floater = [['Dias', 'Bitcoins em USD', 'BRL em USD x 10^-2'],]
+
+        for cont in range(0, len(bitcoin)):
+            floater.append([brl[cont][0], bitcoin[cont][1], brl[cont][1]*100])
+
+        return floater
+
+    except:
+        print("f#$%#@ck")
+
+def get_floating_eur():
+
+    try:
+        eur = get_cambio("EUR",1)
+        bitcoin = get_chart_ord("https://api.blockchain.info/charts/market-price?timespan=1weeks&rollingAverage=1hours&format=json")
+
+        floater = [['Dias', 'Bitcoins em USD', 'EUR em USD x 10^-2'],]
+
+        for cont in range(0, len(bitcoin)):
+            floater.append([eur[cont][0], bitcoin[cont][1], eur[cont][1]*100])
+
+        return floater
+
+    except:
+        print("f#$%#@ck")
+
--- bi_tcoin/app/views.py
+++ bi_tcoin/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app.main import get_stat, get_cambio, get_mkt_share, get_chart, get_get
+from app.main import get_stat, get_cambio, get_mkt_share, get_chart, get_chart_ord, get_floating_cny, get_floating_blr, get_floating_eur
 
 # Create your views here.
 
@@ -22,20 +22,20 @@ def market(request):
 def bitinit(request):
 
     try:
-        context = {"bitcoin": get_stat(),
-                   "cambio": get_cambio(),
-                   "mkt_shr_1d": get_mkt_share("1days"),
-                   "mkt_shr_5d": get_mkt_share("5days"),
-                   "mkt_shr_10d": get_mkt_share("10days"),
-                   "chart_transact_per_sec": get_chart("https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_bitcoin_circ": get_chart("https://api.blockchain.info/charts/total-bitcoins?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_market_price": get_chart("https://api.blockchain.info/charts/market-price?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_orph_blocks": get_chart("https://api.blockchain.info/charts/n-orphaned-blocks?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_hash_rate": get_chart("https://api.blockchain.info/charts/hash-rate?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_difficult": get_chart("https://api.blockchain.info/charts/difficulty?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_cost": get_chart("https://api.blockchain.info/charts/cost-per-transaction-percent?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "chart_wallet": get_chart("https://api.blockchain.info/charts/my-wallet-n-users?timespan=5weeks&rollingAverage=8hours&format=json"),
-                   "var": get_get(),
+        context = {#"bitcoin": get_stat(),
+                   # "cambio": get_cambio(),
+                   "mkt_shr_7d": get_mkt_share("7days"),
+                   "chart_transact_per_sec": get_chart_ord("https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   "chart_bitcoin_circ": get_chart_ord("https://api.blockchain.info/charts/total-bitcoins?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   # "chart_orph_blocks": get_chart_ord("https://api.blockchain.info/charts/n-orphaned-blocks?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   # "chart_hash_rate": get_chart_ord("https://api.blockchain.info/charts/hash-rate?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   # "chart_difficult": get_chart_ord("https://api.blockchain.info/charts/difficulty?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   # "chart_cost": get_chart_ord("https://api.blockchain.info/charts/cost-per-transaction-percent?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   # "chart_wallet": get_chart_ord("https://api.blockchain.info/charts/my-wallet-n-users?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   "floating": get_floating_cny(),
+                   "floating_btc": get_chart_ord("https://api.blockchain.info/charts/market-price?timespan=5weeks&rollingAverage=8hours&format=json"),
+                   "floating_blr": get_floating_blr(),
+                   "floating_eur": get_floating_eur(),
                    }
         return render(request,'bitinit.html',context)
     except:
